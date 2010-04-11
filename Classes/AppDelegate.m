900e2de7d09fde091a81bc36c5e187458dec5e5b
Ricky <rickyc.us@gmail.com>
2010-04-11 15:11:43 -0400

    initial commit

---
+++ Classes/AppDelegate.m
@@ -0,0 +1,46 @@
+//
+//  nytimes_three20_demoAppDelegate.m
+//  nytimes-three20-demo
+//
+//  Created by Ricky Cheng on 4/11/10.
+//  Copyright Family 2010. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+@implementation AppDelegate
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)applicationDidFinishLaunching:(UIApplication *)application {
+  TTNavigator* navigator = [TTNavigator navigator];
+  navigator.persistenceMode = TTNavigatorPersistenceModeAll;
+
+  TTURLMap* map = navigator.URLMap;
+
+  [map from:@"*" toViewController:[TTWebController class]];
+
+  if (![navigator restoreViewControllers]) {
+    [navigator openURLAction:[TTURLAction actionWithURLPath:@"http://three20.info"]];
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)navigator:(TTNavigator*)navigator shouldOpenURL:(NSURL*)URL {
+  return YES;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)application:(UIApplication*)application handleOpenURL:(NSURL*)URL {
+  [[TTNavigator navigator] openURLAction:[TTURLAction actionWithURLPath:URL.absoluteString]];
+  return YES;
+}
+
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  nytimes-three20-demo
+//
+//  Created by Ricky Cheng on 4/11/10.
+//  Copyright Family 2010. All rights reserved.
+//
+
+int main(int argc, char *argv[]) {
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+  [pool release];
+  return retVal;
+}
